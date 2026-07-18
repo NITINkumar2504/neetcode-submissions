ac80ad2860f8792df2eb02f584532ceffe46210b
Nitin Kumar <nk9465110@gmail.com>
2026-07-18 23:49:31 +0530

    Add: reverse-integer - submission-1

---
+++ Data Structures & Algorithms/reverse-integer/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long num = 0;
+        
+        while(x){
+            int digit = x % 10;
+            num = num * 10 + digit;
+            if(num < INT_MIN || num > INT_MAX) return 0;
+
+            x /= 10;
+        }
+
+        return int(num);
+    }
+};
