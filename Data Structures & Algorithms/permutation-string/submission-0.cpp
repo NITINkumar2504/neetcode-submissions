dbc2477a6c1b3b4c681695bb352ed71c08eac44a
Nitin Kumar <nk9465110@gmail.com>
2026-07-28 02:10:52 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
+
+        if(n > m) return false;
+
+        sort(s1.begin(), s1.end());
+
+        for(int i = 0; i <= m - n; i++){
+            string sub = s2.substr(i, n);
+            sort(sub.begin(), sub.end());
+
+            if(s1 == sub) return true;
+        }
+        
+        return false;
+    }
+};
